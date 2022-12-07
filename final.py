7f733c4b410e2bac1a41e8b0d1423b89ef337067
Alex F <afiori@fandm.edu>
2022-12-07 15:12:07 -0500

    updated packages

--- final.py
+++ final.py
@@ -1,4 +1,11 @@
 import streamlit as st 
+import pandas as pd
+import numpy as np
+import altair as alt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 st.markdown("#LinkedIn User Prediction App")
 
